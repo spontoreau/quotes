4fd138934ac1518e1f4fc45118cfc2ecf3c6f45b
spontoreau <pontoreau.sylvain@gmail.com>
2018-11-23 15:30:46 +0100

    Quote data access

--- src/models/quote.ts
+++ src/models/quote.ts
@@ -1,6 +1,43 @@
-export type Quote = {
+import { CosmosClient, SqlQuerySpec } from "@azure/cosmos";
+import { createQuote } from "../actions/quote";
+
+type Quote = Readonly<{
     author: string,
     quote: string,
     language: string,
     moderate: boolean,
+}>;
+
+const getCosmosClient = () : CosmosClient => {
+    return new CosmosClient({ endpoint: "", auth: { masterKey: "" }});
+}
+
+const getCollection = (client: CosmosClient) => (database: string) => (collection: string) => {
+    return client.database(database).container(collection);
 };
+
+const getQuote = async () : Promise<ReadonlyArray<Quote>> => {
+    const querySpec: SqlQuerySpec = {
+        query: "SELECT * FROM root WHERE r.moderate=@moderate",
+        parameters: [{
+            name: "@moderate",
+            value: false
+        }]
+    };
+    const client = getCosmosClient();
+    const collection = getCollection(client)("QuoteDB")("Quote");
+    const { result } = await collection.items.query(querySpec).toArray();
+    return result ? result : [];
+}
+
+const insertQuote = async (quote: Quote) => {
+    const client = getCosmosClient();
+    const collection = getCollection(client)("QuoteDB")("Quote");
+    const { body } = await collection.items.create(quote);
+    return body;
+}
+
+export {
+    Quote,
+    getQuote,
+}
